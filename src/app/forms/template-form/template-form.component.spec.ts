30fba21f820d9e779fabed939ec6b2de5bebca2f
Justin Schwartzenberger <schwarty@gmail.com>
2017-07-17 08:35:52 -0700

    unit test fix

--- src/app/forms/template-form/template-form.component.spec.ts
+++ src/app/forms/template-form/template-form.component.spec.ts
@@ -4,6 +4,7 @@ import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule, MdInputModule, MdSelectModule } from '@angular/material';
+import { By } from '@angular/platform-browser';
 
 describe('TemplateFormComponent', () => {
   let component: TemplateFormComponent;
@@ -33,4 +34,22 @@ describe('TemplateFormComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  // describe('first name field', () => {
+  //   it('should be valid when set to long enough string', () => {
+  //     const firstName = fixture.debugElement.query(By.css('[name=firstName]'));
+  //     firstName.nativeElement.value = 'test';
+  //     fixture.whenStable().then(() => {
+  //       expect(firstName.classes['ng-valid']).toBeTruthy();
+  //     });
+  //   });
+  //
+  //   it('should be invalid when set to string that is too short', () => {
+  //     const firstName = fixture.debugElement.query(By.css('[name=firstName]'));
+  //     firstName.nativeElement.value = 'a';
+  //     fixture.whenStable().then(() => {
+  //       expect(firstName.classes['ng-valid']).toBeFalsy();
+  //     });
+  //   });
+  // });
 });
